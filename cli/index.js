ca90a2b0a64614f08dcab986745175d8fd40ad60
BTMuli <BT-Muli@outlook.com>
2022-09-29 15:47:40 +0800

    feat(v0.2.5): mmd 增加文件已存在判断

--- cli/index.js
+++ cli/index.js
@@ -7,7 +7,7 @@ const MuCli = new Command()
 // Base info
 MuCli
     .name('MuCli')
-    .version('0.2.4', '-v, --version')
+    .version('0.2.5', '-v, --version')
     .description("A Node Cli for Personal Use by BTMUli.");
 
 // Command hello
