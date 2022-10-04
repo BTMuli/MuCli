3eda30d8f6fd077d428977db352cee38a345132a
BTMuli <BT-Muli@outlook.com>
2022-10-04 21:48:54 +0800

    feat(v0.2.9): 版本更新

--- cli/index.js
+++ cli/index.js
@@ -9,7 +9,7 @@ const MuCli = new Command()
 // Base info
 MuCli
     .name('MuCli')
-    .version('0.2.8', '-v, --version')
+    .version('0.2.9', '-v, --version')
     .description("A Node Cli for Personal Use by BTMUli.");
 
 // Commands add
