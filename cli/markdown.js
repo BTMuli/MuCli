551329af6ff3a2b6313bf65e43ff5a08cbab6c05
BTMuli <BT-Muli@outlook.com>
2022-10-10 19:27:52 +0800

    feat(mmd): 新建 mmd typora -n [name]，能够调用本地 Typora 打开文件（需配置路径）

--- cli/markdown.js
+++ cli/markdown.js
@@ -7,7 +7,7 @@ const markdown = new Command();
 markdown
     .name('mmd')
     .description('A SubCommand within MuCli for Markdown')
-    .version('0.2.0', '-sv')
+    .version('0.3.0', '-sv')
 
 // SubCommand for create markdown new file
 markdown
@@ -17,6 +17,24 @@ markdown
     .action(args => {
         let md = new Markdown()
         md.createNew(args.n)
+    })
+
+// SubCommand for open file with Typora - config is needed
+markdown
+    .command('typora')
+    // Using Typora
+    .option('-n [name]', 'open [name] with Typora', '')
+    .description('open file with Typora - config is needed')
+    .action(args => {
+        let md = new Markdown()
+        md.openTypora(args.n)
+    })
+    // Get Typora Info
+    .option('-p, --path','get local typora path')
+    .description('get local typora path')
+    .action(() => {
+        let md = new Markdown()
+        console.log(md.getConfigTypora().path)
     })
 
 export default markdown;
--- utils/markdown.js
+++ utils/markdown.js
@@ -1,4 +1,8 @@
+// Node JS
 import inquirer from "inquirer";
+import {resolve} from 'node:path';
+import {execFile} from "child_process";
+// MuCli JS
 import MarkdownModel from "../config/markdown.js";
 import MucFile from "./file.js";
 import Config from "../config/index.js";
@@ -6,10 +10,29 @@ import Config from "../config/index.js";
 var markdownConfig = new Config().readDetailConfig('Commands', 'mmd')
 
 class Markdown {
+    /**
+     * 初始化构建各种设置
+     */
     constructor() {
+        this.configAll = markdownConfig
         this.authorDefault = markdownConfig.default.author
         this.fileDefault = markdownConfig.default.filename
         this.descDefault = markdownConfig.default.description
+        this.typora = markdownConfig.typora
+        if(this.typora.enable){
+            this.typoraPath = this.typora.path
+        }
+    }
+
+    /*
+     * 获取各种配置
+     */
+    getConfigAll() {
+        return this.configAll
+    }
+
+    getConfigTypora() {
+        return this.typora
     }
 
     /**
@@ -32,6 +55,23 @@ class Markdown {
             var mdPath = answers.title + '.md'
             await mucFile.fileRewriteCheck(mdPath, mdModel.getModel(), inquirer)
         })
+    }
+
+    /**
+     * 调用 Typora 打开文件
+     */
+    openTypora(){
+        if (this.typoraPath){
+            var filePath = resolve() + "\\" + name
+            execFile(this.typoraPath, [filePath], function(err, data) {
+                if (err) {
+                    throw err;
+                }
+                console.log(data.toString());
+            });
+        } else {
+            console.log("Typora 模块未加载！")
+        }
     }
 }
 
