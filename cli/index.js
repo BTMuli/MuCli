c114775fa33c0209e041cc18399ce04559310368
BTMuli <BT-Muli@outlook.com>
2022-10-28 15:46:21 +0800

    chore(set): 先搞了个 muc set
    能够查看初始化子命令加载情况，预想中这个配置应该是全局的。

--- cli/index.js
+++ cli/index.js
@@ -7,20 +7,28 @@ import subCommand from './SubCommand.js';
 import bilibili from './bilibili.js';
 
 const MuCli = new Command();
+let muc = new Config();
 
 // Base info
 MuCli.name('muc')
 	.version('0.3.2', '-v, --version')
 	.description('A Node Cli for Personal Use by BTMUli.');
 
+// Load subCommand and setting
+MuCli.command('set')
+	.option('-n, --name [name]', 'see and set [name]', 'all')
+	.description('change subcommand use status')
+	.action(args => {
+		muc.setConfig(markdown, subCommand, bilibili);
+	});
+
 /**
  * 检验权限然后添加命令
  * @param cmd 命令
  */
 function setCommand(...cmd) {
-	var config = new Config();
 	cmd.forEach(value => {
-		if (config.doConfig(value)) {
+		if (muc.doConfig(value)) {
 			MuCli.addCommand(value);
 		}
 	});
