39f7ce1edac75eaaea32be835190396b742071d0
BTMuli <BT-Muli@outlook.com>
2022-12-02 17:39:46 +0800

    fix(pip): 方法正规化

--- cli/pip.js
+++ cli/pip.js
@@ -1,43 +1,38 @@
 // Node JS
-import { Command } from "commander";
+import { Command } from 'commander';
 // MuCli JS
-import Pip from "../utils/pip.js";
-import { exec } from 'child_process';
+import Pip from '../utils/pip.js';
 
-const pip= new Command();
+const pip = new Command();
 
 // Base info
-pip
-    .name('pip')
-    .description('A SubCommand within MuCli for pip')
-    .version('0.0.1', '-sv')
+pip.name('pip')
+	.description('A SubCommand within MuCli for pip')
+	.version('0.1.0', '-sv');
 
-pip
-	.command('install')
+// 安装包
+pip.command('install')
 	.description('install package')
-	// 无参数接受后面的内容
 	.option('-p, --package [package]', 'install package')
 	.option('-r, --requirement [requirement]', 'install requirement')
 	.action(args => {
-		let sh_str = 'pip install -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com'
-		if(args.package){
-			exec(sh_str + ' ' + args.package, (error, stdout, stderr) => {
-				if (error) {
-					console.error(`执行的错误: ${error}`);
-					return;
-				}
-				console.log(`stdout: ${stdout}`);
-				console.log(`stderr: ${stderr}`);
-			});
-		}else if(args.requirement){
-			exec(sh_str + ' -r ' + args.requirement, (error, stdout, stderr) => {
-				if (error) {
-					console.error(`执行的错误: ${error}`);
-					return;
-				}
-				console.log(`stdout: ${stdout}`);
-				console.log(`stderr: ${stderr}`);
-			});
-		}
+		let pip = new Pip();
+		pip.install(args);
+	});
+
+// 测试镜像源
+pip.command('test')
+	.description('test mirror')
+	.action(async () => {
+		let pip = new Pip();
+		await pip.verifyMirror();
+	});
+
+// 查看镜像源
+pip.command('show')
+	.description('show mirror')
+	.action(() => {
+		let pip = new Pip();
+		pip.showMirror();
 	});
 export default pip;
--- config/pip.js
+++ config/pip.js
@@ -1,5 +1,101 @@
-class PipModel {
+import axios from 'axios';
 
+/**
+ * 镜像模型
+ */
+export class MirrorModel {
+	/**
+	 * 构造函数
+	 * @param mirror 镜像
+	 */
+	constructor(mirror) {
+		this.name = mirror.name;
+		this.url = mirror.url;
+		if (mirror.usable === true || mirror.usable === false) {
+			this.usable = mirror.usable;
+		} else {
+			// 默认不可用
+			this.usable = false;
+		}
+	}
+
+	/**
+	 * 获取镜像模型
+	 */
+	getModel() {
+		return {
+			name: this.name,
+			url: this.url,
+			usable: this.usable,
+		};
+	}
+
+	/**
+	 * 测试镜像地址是否可用
+	 */
+	async verifyMirror(url = undefined) {
+		if (url === undefined) {
+			url = this.url;
+		}
+		return await axios
+			.get(url)
+			.then(response => {
+				return response.status === 200;
+			})
+			.catch(error => {
+				console.log(error);
+				return false;
+			});
+	}
+
+	/**
+	 * 获取镜像地址
+	 */
+	getMirrorUrl() {
+		return this.url;
+	}
+
+	/**
+	 * 获取镜像名称
+	 */
+	getMirrorName() {
+		return this.name;
+	}
+
+	/**
+	 * 更新镜像地址
+	 */
+	updateMirrorUrl(url) {
+		this.url = url;
+	}
+
+	/**
+	 * 输出镜像信息
+	 */
+	outputMirrorInfo() {
+		console.log(
+			// 左对齐
+			`镜像名称：${this.name}，镜像地址：${this.url}，是否可用：${this.usable}`
+		);
+	}
 }
 
-export default PipModel;
+export class PipModel {
+	constructor(useMirror, mirrorList) {
+		// 镜像源是否采用
+		this.useMirror = useMirror;
+		// 镜像源地址
+		// {name: '清华', url: 'https://pypi.tuna.tsinghua.edu.cn/simple'}
+		this.mirrorList = mirrorList;
+	}
+
+	/**
+	 * 获取所有镜像源信息
+	 * @return {Array<MirrorModel>} 镜像源信息
+	 */
+	getMirrorList() {
+		this.mirrorList.forEach(mirror => {
+			mirror.outputMirrorInfo();
+		});
+	}
+}
