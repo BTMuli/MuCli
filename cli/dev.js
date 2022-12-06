8b707cfdde2e3a71810cb2ea5b6bb9b607d6a620
BTMuli <BT-Muli@outlook.com>
2022-12-06 22:56:20 +0800

    fix(dev): 重构 config/dev

--- cli/dev.js
+++ cli/dev.js
@@ -16,14 +16,16 @@ const DevVersion = PROJECT_INFO['subversion']['dev'];
 const dev = new Command();
 
 /* 基本信息 */
-dev
-	.name('dev')
+dev.name('dev')
 	.description('A SubCommand within MuCli for SubCommand')
-	.version(DevVersion, '-sv, --subversion', 'output the subversion of MuCli-Dev');
+	.version(
+		DevVersion,
+		'-sv, --subversion',
+		'output the subversion of MuCli-Dev'
+	);
 
 /* 创建子命令 */
-dev
-	.command('new')
+dev.command('new')
 	.option('-c, --command [command]', 'command name')
 	.action(options => {
 		let muc = new Dev();
