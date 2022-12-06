65c4db262c2f04e6f1c9b02c64af5d4b05c3874e
BTMuli <BT-Muli@outlook.com>
2022-12-06 23:04:54 +0800

    fix(dev): 重构utils/dev

--- utils/dev.js
+++ utils/dev.js
@@ -1,12 +1,19 @@
-// Node JS
-import inquirer from 'inquirer';
-// MuCli JS
-import MucFile from './file.js';
-import SubCommandModel from '../config/SubCommand.js';
+/**
+ * @author: BTMuli<bt-muli@outlook.com>
+ * @date: 2022-12-06
+ * @description: 子命令 dev 的具体实现
+ * @update: 2022-12-06
+ */
 
-class SubCommand {
+/* Node */
+import inquirer from 'inquirer';
+/* MuCli */
+import MucFile from './file.js';
+import DevModel from '../config/dev.js';
+
+class Dev {
 	createNew(name) {
-		var mucFile = new MucFile();
+		let mucFile = new MucFile();
 		inquirer
 			.prompt([
 				{
@@ -29,21 +36,17 @@ class SubCommand {
 				},
 			])
 			.then(async answers => {
-				var subC = new SubCommandModel(
+				let dev = new DevModel(
 					answers.name,
 					answers.command,
 					answers.desc
 				);
-				var paths = subC.getFilesPath();
-				for (var p in paths) {
-					await mucFile.fileRewriteCheck(
-						paths[p],
-						subC.getModel(p),
-						inquirer
-					);
+				const paths = dev.getFilesPath();
+				for (const p in paths) {
+					await mucFile.writeFile(paths[p], dev.getModel(p));
 				}
 			});
 	}
 }
 
-export default SubCommand;
+export default Dev;
