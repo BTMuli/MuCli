2aebdf0746872ed4b4b72df6eff616e199d1836f
BTMuli <BT-Muli@outlook.com>
2022-12-06 17:51:51 +0800

    fix(mmd): 重构 cli/markdown

--- cli/markdown.js
+++ cli/markdown.js
@@ -1,41 +1,56 @@
-// Node JS
+/**
+ * @author: BTMuli<bt-muli@outlook.com>
+ * @date: 2022-12-06
+ * @description: 子命令，负责处理 markdown 文件
+ * @update: 2021-12-06
+ */
+
+/* Node */
 import { Command } from 'commander';
-// MuCli JS
+/* MuCli */
 import Markdown from '../utils/markdown.js';
+import { PROJECT_INFO } from '../config.js';
+
+/* 版本管理 */
+const MmdVersion = PROJECT_INFO['subversion']['mmd'];
 
 const markdown = new Command();
 
-// Base info
+/* 基本信息 */
 markdown
 	.name('mmd')
 	.description('A SubCommand within MuCli for Markdown')
-	.version('0.3.2', '-sv');
+	.version(
+		MmdVersion,
+		'-sv, --subversion',
+		'output the subversion of MuCli-Markdown'
+	);
 
-// SubCommand for create markdown new file
+/* 建立新的 markdown 文件 */
 markdown
 	.command('new')
-	.option('-n [name]', 'new markdown file [name]', 'README')
+	.option('-n, --name <name>', 'the name of the new markdown file', 'README')
 	.description('create a markdown file')
-	.action(args => {
+	.action(options => {
 		let md = new Markdown();
-		md.createNew(args.n);
+		md.createNew(options.name);
 	});
 
-// SubCommand for open file with Typora - config is needed
+/* Typora 相关 */
 markdown
 	.command('typora')
-	// Using Typora
-	.option('-n [name]', 'open [name] with Typora', '')
+	/* 通过 Typora 打开 markdown 文件 */
+	.option('-n, --name [name]', 'the name of the markdown file', './')
 	.description('open file with Typora')
-	// Get Typora Info
+	/* 获取 Typora 的路径 */
 	.option('-p, --path', 'get local typora path')
 	.description('get local typora path')
-	.action(args => {
+	.action(options => {
 		let md = new Markdown();
-		if (args.path) {
+		if (options.path) {
 			console.log(md.getConfigTypora().path);
-		} else if (args.n) {
-			md.openTypora(args.n);
+		} else if (options.name) {
+			md.openTypora(options.name);
 		}
 	})
 	.description('using local Typora - config is needed');
