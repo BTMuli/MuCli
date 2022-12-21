da43f90ae8019d75d925ff6ed0b9bba579b084b3
BTMuli <BT-Muli@outlook.com>
2022-12-21 11:50:27 +0800

    fix(mmd): FrontMatter 获取跟更新

--- config/markdown.js
+++ config/markdown.js
@@ -7,6 +7,8 @@
 
 /* Node */
 import { format } from 'silly-datetime';
+/* MuCli */
+import MucFile from '../utils/file.js';
 
 class MarkdownModel {
 	sign = '---';
@@ -25,7 +27,7 @@ class MarkdownModel {
 	 * @description 获取默认写入内容
 	 * @return {string}
 	 */
-	getLabel() {
+	getHeader() {
 		const dateNow = format(new Date(), 'YYYY-MM-DD');
 		return (
 			this.sign + this.lineBreak +
@@ -34,9 +36,80 @@ class MarkdownModel {
             'Description: ' + this.description + this.lineBreak +
 			'Update: ' + dateNow + this.lineBreak +
             this.sign + this.lineBreak + this.lineBreak +
-            this.quote + this.lineBreak +
-            '`' + format(new Date(), 'YYYY-MM-DD HH:mm:ss') + '`' + this.lineBreak
+            this.quote + '`' + format(new Date(), 'YYYY-MM-DD HH:mm:ss') + '`' + this.lineBreak +
+			'> ' + this.lineBreak +
+			'> 更新于 `' + format(new Date(), 'YYYY-MM-DD HH:mm:ss') + '`'
 		);
+	}
+	/**
+	 * @description 读取文件FrontMatter（确定已存在）
+	 * @param {string} fileName 文件名
+	 * @return {
+	 * Promise<{
+	 * 	header:{
+	 * 		author:string,
+	 * 		date:string,
+	 * 		description:string,
+	 * 		update:string},
+	 * 	quote:{
+	 * 		date:string,
+	 * 		update:string}
+	 * 	}>} FrontMatter
+	 */
+	async readHeader(fileName) {
+		const headContent = await new MucFile().readLine(fileName, 10);
+		return {
+			header: {
+				author: headContent[1].split(':')[1].trim(),
+				date: headContent[2].split(':')[1].trim(),
+				description: headContent[3].split(':')[1].trim(),
+				update: headContent[4].split(':')[1].trim(),
+			},
+			quote: {
+				date: headContent[7].split('`')[3],
+				update: headContent[9].split('`')[1],
+			}
+		};
+	}
+	/**
+	 * @description 更新文件FrontMatter
+	 * @param {string} fileName 文件名
+	 * @return {
+	 * Promise<{
+	 * 	header:{
+	 * 		author:string,
+	 * 		date:string,
+	 * 		description:string,
+	 * 		update:string},
+	 * 	quote:{
+	 * 		date:string,
+	 * 		update:string}
+	 * 	}>} FrontMatter
+	 */
+	async updateHeader(fileName) {
+		let headRead = await this.readHeader(fileName);
+		headRead.header.update = format(new Date(), 'YYYY-MM-DD');
+		headRead.quote.update = format(new Date(), 'YYYY-MM-DD HH:mm:ss');
+		return headRead;
+	}
+	/**
+	 * @description 写入文件FrontMatter
+	 * @param {string} fileName 文件名
+	 * @return {Promise<string>} 写入内容
+	 */
+	async writeHeader(fileName) {
+		const labelUpdate =await this.updateHeader(fileName);
+		return (
+			this.sign + this.lineBreak +
+			'Author: ' + labelUpdate.header.author + this.lineBreak +
+            'Date: ' + labelUpdate.header.date + this.lineBreak +
+            'Description: ' + this.description + this.lineBreak +
+			'Update: ' + labelUpdate.header.update + this.lineBreak +
+            this.sign + this.lineBreak + this.lineBreak +
+            this.quote + '`' + labelUpdate.quote.date + '`' + this.lineBreak +
+			'> ' + this.lineBreak +
+			'> 更新于 `' + labelUpdate.quote.update + '`'
+		)
 	}
 }
 
