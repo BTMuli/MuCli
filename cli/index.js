7762eeac6491b45a30ed7efb07e51ab00f0777b7
BTMuli <BT-Muli@outlook.com>
2023-02-15 15:07:25 +0800

    feat(0.6.4)：版本更新

--- cli/index.js
+++ cli/index.js
@@ -7,6 +7,9 @@
 
 /* Node */
 import { Command } from 'commander';
+import { exec } from 'node:child_process';
+import axios from 'axios';
+import inquirer from 'inquirer';
 /* MuCli */
 import { PROJECT_INFO } from '../config.js';
 import Config, { COMMAND_LIST } from '../config/index.js';
@@ -55,6 +58,48 @@ MuCli.command('set')
 		muc.transConfig(options.name, options.target);
 		/* 读取配置 */
 		muc.loadConfig(MuCli);
+	});
+
+/* 读取上游更新 */
+MuCli.command('update')
+	.description('update muc from upstream')
+	.action(() => {
+		console.log(`\n当前版本：${MuCliVersion}`);
+		console.log(`正在检查更新...`);
+		axios.get('https://registry.npmjs.org/@btmuli/mucli').then(res => {
+			const upStreamVersion = res.data['dist-tags'].latest;
+			console.log(`上游版本：${upStreamVersion}`);
+			// 比较版本号
+			if (MuCliVersion < upStreamVersion) {
+				inquirer
+					.prompt([
+						{
+							type: 'confirm',
+							name: 'update',
+							message: '是否更新？',
+							default: true,
+						},
+					])
+					.then(async answer => {
+						if (answer.update) {
+							await exec(
+								'npm i -g @btmuli/mucli',
+								(err, stdout, stderr) => {
+									if (err) {
+										console.log(err);
+										return;
+									}
+									console.log(stdout);
+								}
+							);
+						} else {
+							console.log(`已取消更新`);
+						}
+					});
+			} else {
+				console.log(`已是最新版本`);
+			}
+		});
 	});
 
 export default MuCli;
