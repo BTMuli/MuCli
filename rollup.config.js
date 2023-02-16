f6060261dc774152bbb603f3489eba850f7ee484
BTMuli <BT-Muli@outlook.com>
2023-02-16 15:20:33 +0800

    style(rollup)：代码格式化，添加注释

--- rollup.config.js
+++ rollup.config.js
@@ -1,41 +1,47 @@
-import typescript from 'typescript'
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import json from '@rollup/plugin-json'
-import terser from '@rollup/plugin-terser'
-import typescript2 from 'rollup-plugin-typescript2'
+/**
+ * @author BTMuli<bt-muli@outlook.com>
+ * @description rollup config
+ * @version 0.7.0
+ */
 
-import { dependencies } from './package.json'
+/* Node */
+import typescript from "typescript";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import json from "@rollup/plugin-json";
+import terser from "@rollup/plugin-terser";
+import typescript2 from "rollup-plugin-typescript2";
+import { dependencies } from "./package.json";
 
-const external = Object.keys(dependencies || '')
+const external = Object.keys(dependencies || "");
 const globals = external.reduce((prev, current) => {
-	const newPrev = prev
+	const newPrev = prev;
 
-	newPrev[current] = current
-	return newPrev
-}, {})
+	newPrev[current] = current;
+	return newPrev;
+}, {});
 
 const defaultConfig = {
-	input: './src/index.ts',
+	input: "./src/index.ts",
 	output: {
-		file: './dist/index.js',
-		format: 'esm',
-		banner: '#!/usr/bin/env node',
-		globals
+		file: "./dist/index.js",
+		format: "esm",
+		banner: "#!/usr/bin/env node",
+		globals,
 	},
 	external,
 	plugins: [
 		nodeResolve({
-			extensions:['.js', '.ts']
+			extensions: [".js", ".ts"],
 		}),
 		typescript2({
-			exclude: 'node_modules/**',
+			exclude: "node_modules/**",
 			useTsconfigDeclarationDir: true,
 			typescript,
-			tsconfig: './tsconfig.json'
+			tsconfig: "./tsconfig.json",
 		}),
 		json(),
-		terser()
-	]
-}
+		terser(),
+	],
+};
 
-export default defaultConfig
+export default defaultConfig;
