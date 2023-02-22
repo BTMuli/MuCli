31a5f465336a32f68f19cb7dc16c6e9353ff29cf
BTMuli <BT-Muli@outlook.com>
2023-02-22 12:13:05 +0800

    refactor(config): 根目录下的 config 调整

--- src/config.ts
+++ src/config.ts
@@ -1,20 +1,20 @@
 /**
  * @author BTMuli<bt-muli@outlook.com>
  * @description 处理根目录相关功能
- * @version 0.7.0
+ * @version 0.7.2
  */
 
 /* Node */
 import { dirname, join } from "path";
 import { readFileSync } from "fs";
 import { fileURLToPath } from "url";
-import { ProjPackageJson } from "./utils/interface";
+import { PackageJson } from "./interface";
 
 /* 项目根目录 */
 export const ROOT_PATH: string = dirname(
 	dirname(fileURLToPath(import.meta.url))
 );
 /* 项目包信息 */
-export const PROJECT_INFO: ProjPackageJson = JSON.parse(
+export const PROJECT_INFO: PackageJson = JSON.parse(
 	readFileSync(join(ROOT_PATH, "package.json")).toString()
 );
